cfae7fb9f56b3cc49517cb0eb846c2652d046aa2
Raeth <raethandres4@gmail.com>
2020-12-17 11:50:10 -0500

    up version

--- mongoschemas/activity.js
+++ mongoschemas/activity.js
@@ -146,4 +146,4 @@ module.exports=new mongoose.Schema({
 	link:{
 		type:String
 	},
-},{bufferCommands:false,collection:"activities",timestamps:true,versionKey:false,_id:false,id:false}).index({order:1}).index({subcategories:1}).index({moods:1}).index({createdAt:1});
+},{bufferCommands:false,collection:"activities",timestamps:true,versionKey:false,_id:false,id:false}).index({order:1}).index({subcategories:1}).index({title:1}).index({moods:1}).index({createdAt:1});
